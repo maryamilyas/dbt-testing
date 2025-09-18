691e1d3c98c48772f8664e2fa1e0ee416eb7fe2a
Maryam Ilyas <maryam@dataroots.io>
2025-09-18 08:45:36 +0000

    writing tests

--- models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -2,4 +2,4 @@
 select
     id as customer_id,
     name
-from raw.jaffle_customers
+from {{source('jaffle_shop','customers')}}
--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -4,4 +4,4 @@ select
     customer as customer_id,
     ordered_at as order_date,
     order_total
-    from raw.jaffle_orders
+    from from {{source('jaffle_shop','orders')}}
